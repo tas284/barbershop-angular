276a0da3c6bc1fcc25eaa2df2a191804e89847ec
tas284 <tiago.avila.saldanha@gmail.com>
2023-06-23 00:27:31 -0300

    Add Datepicker

---
+++ src/app/shared/components/datepicker/datepicker/datepicker.component.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatNativeDateModule} from '@angular/material/core';
+
+@Component({
+  selector: 'app-datepicker',
+  templateUrl: './datepicker.component.html',
+  styleUrls: ['./datepicker.component.scss'],
+  standalone: true,
+  imports: [MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule],
+})
+export class DatepickerComponent { }
